c7db2fd354822485f62c33816fd49216ce56a5d6
Vitor Franca <vitorfranca089@gmail.com>
2025-03-13 16:33:08 -0300

    feat: resolução do desafio

--- src/ParametrosInvalidosException.java
+++ src/ParametrosInvalidosException.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class ParametrosInvalidosException {
+public class ParametrosInvalidosException extends RuntimeException{
+
+    private static final String MENSAGEM_ERRO = "O segundo parâmetro deve ser maior que o primeiro";
+
+    public ParametrosInvalidosException() {
+            super(MENSAGEM_ERRO);
+    }
+
 }
